f833e157ea85c22953883895899f2d7b51d8a22a
ValentinaNovarino <valenovarino@gmail.com>
2020-11-06 16:33:20 +0100

    prove click vue

--- main.js
+++ main.js
@@ -5,7 +5,17 @@ var app = new Vue(
             // stampo a schermo un messaggio
             messagge: 'Buona Serata!',
             // BONUS stampo a schermo un immagine
-            image_url: 'https://img.freepik.com/free-vector/space-background-with-abstract-shape-stars_189033-30.jpg?size=626&ext=jpg'
+            imageUrl: 'https://img.freepik.com/free-vector/space-background-with-abstract-shape-stars_189033-30.jpg?size=626&ext=jpg',
+            filterImg: 'normal-img'
+        },
+        methods: {
+            changeFilter() {
+                if (this.filterImg == 'normal-img') {
+                    this.filterImg = 'filtered-img';
+                } else {
+                    this.filterImg = 'normal-img';
+                }
+            }
         }
     }
 
