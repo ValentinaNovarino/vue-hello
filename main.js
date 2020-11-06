783e504022d2cd36ead81e78e01a7f9dd6ca6f88
ValentinaNovarino <valenovarino@gmail.com>
2020-11-06 16:04:16 +0100

    esercizio completato

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            // stampo a schermo un messaggio
+            messagge: 'Buona Serata!',
+            // BONUS stampo a schermo un immagine
+            image_url: 'https://img.freepik.com/free-vector/space-background-with-abstract-shape-stars_189033-30.jpg?size=626&ext=jpg'
+        }
+    }
+
+);
